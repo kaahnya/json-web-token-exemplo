415ed373faa30e3c88bbe54512ac0c3f104f5cd9
kaahnya <karina.decleva@estudante.ifms.edu.br>
2023-08-25 17:29:21 -0400

    commit

--- servidor/index.js
+++ servidor/index.js
@@ -33,20 +33,16 @@ app.get('/usuarios/cadastrar', async function(req, res){
 })
 
 app.post('/usuarios/cadastrar', async function(req, res){
-  if(req.body.senha === req.body.senhaagain)
-  res.json({mensagem: "Cadastro realizado!"})
-else(
-  res.json({mensagem: "Senhas não são iguais!"})
- )
-});
-
-app.post('/usuarios/cadastrar', async function(req, res){
-  if(req.body.senha === req.body.senhaagain)
-  res.json({mensagem: "Cadastro realizado!"})
-else(
-  res.json({mensagem: "Senhas não são iguais!"})
- )
-});
+  try {
+    if(req.body.senha == req.body.senhaagain){
+      await usuario.create(req.body);
+      res.redirect('/usuarios/listar')
+    }
+} catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'As senhas não são iguais!✧' });
+}
+})
 
 app.get('/autenticar', async function(req, res){
   res.render('autenticar');
@@ -90,8 +86,14 @@ app.post('/usuarios/cadastrar', async function(req, res){
 })
 
 app.get('/usuarios/listar', async function(req, res){
-  res.json('usuarios')
-})
+  try {
+   var usuarios = await usuario.findAll();
+   res.render('home', { usuarios });
+ } catch (err) {
+   console.error(err);
+   res.status(500).json({ message: 'Ocorreu um erro ao buscar os usuário.' });
+ }
+ })
 
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!')
