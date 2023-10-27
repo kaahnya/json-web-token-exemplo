6ae7fe1d82ac5cf2c05211185163d9b00e42c699
kakah <karina.decleva@estudante.ifms.edu.br>
2023-10-26 22:25:05 -0300

    commit autenticar

--- servidor/index.js
+++ servidor/index.js
@@ -68,20 +68,21 @@ app.get('/usuarios/listar', async function(req, res){
 }
 })
 
-app.post('/logar', async (req, res) => {
-  if(req.body.usuario == 'kakah' && req.body.senha == '123') {
-    const id = 1;
+app.post('/logar', async function(req, res) {
+  const login = await usuario.findOne({ where: { nome: req.body.nome, senha: crypto.encrypt(req.body.senha) } })
+  if(login){
+    const id = login.id;
     const token = jwt.sign({ id }, process.env.SECRET, {
-      expiresIn: 300
+      expiresIn: 3000
     })
-    
     res.cookie('token', token, {httpOnly:true});
     return res.json({
-      usuario: req.body.usuario,
+      nome: req.body.nome,
+      senha: crypto.encrypt(req.body.senha),
       token: token
     })
   }
-    res.status(500).json({mensagem: "Login inválido!"})
+    res.status(500).json({mensagem: "login invalido!"})
 })
 
 app.post('/deslogar', function(req, res) {
--- servidor/seeders/20220727021551-adicionar-usarios.js
+++ servidor/seeders/20220727021551-adicionar-usarios.js
@@ -1,10 +1,9 @@
 'use strict';
-const crypto = require('.../servidor/crypto');
+const crypto = require('../crypto');
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('usuarios', [
-      { nome: 'John Doe', senha: '123' },
-      { nome: 'Picolo', senha: crypto.encrypt('123')}
+      { nome: 'picolo', senha: crypto.encrypt('123')}
     ], {});
   },
 
